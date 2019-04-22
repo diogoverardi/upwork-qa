a4354d66d3efedf8946fd1f7c063b9aa1ba65273
diogo-foa <diogo.verardi@foa.com.py>
2019-04-22 10:38:26 -0300

    added comments on methods in the ProfileBrowse method

--- resources/pages/profiles_browse.rb
+++ resources/pages/profiles_browse.rb
@@ -1,5 +1,7 @@
 require_relative 'base_page'
 
+##
+# Class responsible for interactions in the Freelancer Search Page
 class ProfilesBrowse < BasePage
 
   def initialize(browser)
@@ -15,6 +17,11 @@ class ProfilesBrowse < BasePage
   PROFILE_CARD_SECTION_LOCATOR  = { css: '#oContractorResults > div > div > section.air-card-hover' }.freeze
 
 
+  # Goes through each search result,
+  # gets the desirable data and
+  # adds to a class attribute @profiles_data
+  #
+  # Returns a key-value array with each profile data parsed
   def parse_results
     #TODO: log an error if the return is empty
     profiles_array = find_elements PROFILE_CARD_SECTION_LOCATOR
@@ -27,31 +34,35 @@ class ProfilesBrowse < BasePage
               rate:         get_element_text(PROFILE_RATE_LOCATOR, profile)
           }
     end
-
     @profiles_data
   end
 
+  # Return a random profile name
   def get_random_profile_name
-    Log.step 'Getting a random profile'
     @profiles_data[rand(0...@profiles_data.count)][:name]
   end
 
+  #TODO: move the locator to a constant
+  # Click on the given profile name using it's name as a locator
   def click_on_profile_by_name(name = get_random_profile_name)
     click(css: "h4>a[class='freelancer-tile-name'][title='#{name}']")
-    Log.step "Clicked on random profile, #{name} was the chosen one."
+    Log.step "Clicked on random profile, profile selected: '#{name}'"
     wait 5
   end
 
   def verify_profiles_with_keyword(keyword)
-    Log.step "Verifying which profiles contain the keyword: #{keyword}"
+    Log.step "Verifying all profiles for matching keyword: #{keyword}"
 
+    # makes the keyword string downcase for comparison
     keyword = keyword.downcase
 
+    # iterates each profile in the search result
     @profiles_data.each do |profile|
 
       Log.info "||||||||||"
       Log.info "Searching '#{profile[:name]}'"
 
+      # iterate all the data inside each profile
       profile.each do |key, value|
 
         # don't verify the keyword on the profile's name
@@ -62,6 +73,7 @@ class ProfilesBrowse < BasePage
         # downcase the string for more accurate comparison
         value = value.downcase
 
+        # check whether the current profile data contains the keyword in it
         if value.include? keyword
           Log.info "#{key} does include the keyword"
         else
@@ -69,6 +81,7 @@ class ProfilesBrowse < BasePage
         end
 
       end
+
     end
 
   end
