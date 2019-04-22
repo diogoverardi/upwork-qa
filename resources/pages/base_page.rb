46d1e1d824a492fff7bffecff5ab0fb662450f48
diogo-foa <diogo.verardi@foa.com.py>
2019-04-22 10:31:50 -0300

    added methods comments on BasePage module

--- resources/pages/base_page.rb
+++ resources/pages/base_page.rb
@@ -1,5 +1,8 @@
 require_relative '../log/log'
 
+##
+# Class that all pages should inherit from
+# it contains basic methods that all the other pages will use
 class BasePage
 
   attr_accessor :browser
@@ -8,53 +11,55 @@ class BasePage
     @browser = browser
   end
 
-  # clear all the cookies
+  # Clear all the cookies
   def delete_all_cookies
-    Log.step 'Cleaning all the browser cookies'
     @browser.manage.delete_all_cookies
+    Log.step 'Cleaning all browser cookies'
   end
 
-  # goes to the url
+  # Goes to the given url
   def get_page(url)
     @browser.get url
-    Log.step "Going to #{url}"
+    Log.step "Going to '#{url}' url"
   end
 
-  # finds an element by it's locator and returns it's text
-  def get_element_text(locator, wrapper)
+  # Finds an element by it's given locator
+  # and returns it's text
+  def get_element_text(locator, wrapper = nil)
     find(locator, wrapper).text
   end
 
-  # click on the given element
+  # Click on the element by it's locator
   def click(locator)
     find(locator).click
   end
 
-  # find an element by it's locator
+  # Find an element by it's locator
   def find(locator, wrapper = nil)
-    @browser.find_element locator
+    # @browser.find_element locator
     (wrapper ? wrapper : @browser).find_element locator
   end
 
-  # find multiples elements by the same locator
+  # Find multiples elements by it's given locator
+  # and returns it
   def find_elements(locator)
     @browser.find_elements locator
   end
 
-  # test-case sleeps for a request period of time(seconds)
+  # Stop the script execution by a given time(seconds)
   def wait(seconds)
     sleep(seconds)
-    warn "Waiting #{seconds}..."
+    warn "Waiting #{seconds} seconds..."
   end
 
-  # find an element and types a text on it
+  # Find an element by it's locator
+  # and type a text on it
   def find_and_type(locator, text)
     element = find locator
     element.send_keys text
-    Log.step "Typing '#{text}' onto element: '#{locator}' "
   end
 
-  # get the current url
+  # Returns the current browser url
   def get_current_url
     @browser.current_url
   end
