cd87239b10556f5b7c09467497010473bd01c996
diogo-foa <diogo.verardi@foa.com.py>
2019-04-22 10:36:08 -0300

    added comment on methods in the HomePage class

--- resources/pages/home_page.rb
+++ resources/pages/home_page.rb
@@ -1,5 +1,7 @@
 require_relative 'base_page'
 
+##
+# Class responsible for all interactions in the Home Page
 class HomePage < BasePage
 
   NAVBAR_SEARCH_INPUT_LOCATOR   = { css: '#layout > nav > div > div.navbar-collapse.d-none.d-lg-flex > div.navbar-form > form > div.input-group.input-group-search-dropdown.input-group-navbar > input.form-control' }.freeze
@@ -10,7 +12,7 @@ class HomePage < BasePage
     search_for_keyword keyword
     wait 1
     click NAVBAR_SEARCH_BUTTON_LOCATOR
-    wait 4
+    # wait 4
   end
 
   def focus_onto_search_input
@@ -20,6 +22,7 @@ class HomePage < BasePage
 
   def search_for_keyword(keyword)
     find_and_type NAVBAR_SEARCH_INPUT_LOCATOR, keyword
+    Log.step "Typing '#{keyword}' onto navbar search input "
   end
 
 end
