244145cf0cd93d16400d43983b549a23ae2277cb
diogo-foa <diogo.verardi@foa.com.py>
2019-04-22 21:05:27 -0300

    added the hability to set the browser/keyword when executing the script via terminal, as a parameter | or it can be set in the config file

--- test_cases/find_freelancers.rb
+++ test_cases/find_freelancers.rb
@@ -2,7 +2,9 @@ require_relative '../resources/driver/initializer'
 require_relative '../resources/pages/home_page'
 require_relative '../resources/pages/profiles_browse'
 require_relative '../resources/pages/profile_user'
+require_relative '../resources/config/find_freelancers'
 
+# ruby test_cases/find_freelancers.rb "chrome" "selenium"
 
 
 ##############################################################################
@@ -31,10 +33,23 @@ require_relative '../resources/pages/profile_user'
 #
 # Check whether at least one attribute contains <keyword>
 ##############################################################################
+#
+#
 
+
+# Set the browser and keyword to be used on this session
+def get_browser(browser)
+  if browser == 'chrome'
+    :chrome
+  end
+  :firefox
+end
+
+browser_arg = (ARGV[0] ? get_browser(ARGV[0]) :DEFAULT_BROWSER)
+keyword     = (ARGV[1] ? ARGV[1] : DEFAULT_KEYWORD)
 
 # initialize the browser driver
-browser = Initializer.init_browser(:firefox)
+browser = Initializer.init_browser(browser_arg)
 
 # creates an instance of the home_page
 page = HomePage.new browser
@@ -46,7 +61,8 @@ page.delete_all_cookies
 page.get_page 'http://www.upwork.com'
 
 # search for profiles with specific keyword
-page.search_for_freelancers 'magento'
+page.search_for_freelancers keyword
+
 
 
 
@@ -56,7 +72,7 @@ profiles_search_page = ProfilesBrowse.new browser
 search_results = profiles_search_page.parse_results
 
 # check which profiles have the keyword on it
-profiles_search_page.verify_profiles_with_keyword 'magento'
+profiles_search_page.verify_keyword_on_profiles keyword
 
 # click on a random profile and get into it
 profiles_search_page.click_on_profile_by_name
@@ -65,10 +81,14 @@ profiles_search_page.click_on_profile_by_name
 
 
 profile_user_page = ProfileUser.new browser
-# profile_user_page.is_a_company_page
-profile_user_page.verify_data_from_search search_results
+
 # Check that each attribute value is equal to one of those stored in the structure created in #67
-# profile_user_page.verify_data_from_search search_results
-#
-#
-# TODO: add close broswer and log final report
+profile_user_page.verify_data_from_search search_results
+
+# Check whether at least one attribute contains <keyword>
+profile_user_page.verify_keyword_on_profile keyword
+
+# ends the test-case
+profile_user_page.quit
+
+# TODO: log final report, like errors, count steps, stuff like that
