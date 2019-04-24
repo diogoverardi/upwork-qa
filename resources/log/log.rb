43be198966d1fa517c81a4684f237be8b63294d3
Diogo Verardi <verardidiogo@gmail.com>
2019-04-24 02:01:58 -0300

    added a method to show the log counts, such as, errors and steps at the end of the TestCase

--- resources/log/log.rb
+++ resources/log/log.rb
@@ -34,4 +34,35 @@ class Log
     @log.info message
   end
 
+  # Prints the final counts
+  def self.print_counts
+    info 'Final Log Counts'
+    info "Steps #{get_steps_count}"
+    info "Errors #{get_errors_count}"
+  end
+
+
+
+  private
+
+  # Decreases one from the count
+  # since the last interaction will add +1 to the count
+  # and it doesn't decreases if the count hasn't started yet
+  def self.get_steps_count
+    if @step_count == 0
+      return @step_count
+    end
+    @step_count-1
+  end
+
+  # Decreases one from the count
+  # since the last interaction will add +1 to the count
+  # and it doesn't decreases if the count hasn't started yet
+  def self.get_errors_count
+    if @error_count == 0
+      return @error_count
+    end
+    @error_count-1
+  end
+
 end
